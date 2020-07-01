884f5d7406f0f6ebfd3316da0c5af9db62708d8e
VimfulDang <61068522+VimfulDang@users.noreply.github.com>
2020-07-01 12:32:24 -0700

    Updated Pointer Error and Multi Read Function
    Void * was byte addressing so word offset needed to be * 4. Changed virtual base to (volatile unsigned int *) to match word offset in header.
    Multi Read function had an error in logical function.

--- ADXL345_DE1_SOC.c
+++ ADXL345_DE1_SOC.c
@@ -14,12 +14,15 @@
 #define ADXL345_THRESH_INACT		0x25
 #define ADXL345_TIME_INACT			0x26
 #define ADXL345_ACT_INACT_CTL		0x27
+#define ADXL345_INT_ENABLE			0x2E
 #define ADXL345_INT_SOURCE			0x30
 #define ADXL345_DATA_FORMAT			0x31
+#define ADXL345_ACTIVITY			0x10
+#define ADXL345_DATAREADY 			0x80
 
-static unsigned int * base_ptr;
-static void * i2c0base;
-static int fd_i2c0base = -1;
+static unsigned int * i2c0_base_ptr, * sysmgr_base_ptr;
+static void * i2c0base_virtual, * sysmgrbase_virtual;
+static int fd_i2c0base = -1, fd_sysmgr = -1;
 
 int open_physical(int);
 void * map_physical (int, unsigned int, unsigned int);
@@ -32,24 +35,22 @@ void ADXL345_REG_WRITE(uint8_t, uint8_t );
 void ADXL345_REG_MULTI_READ(uint8_t address, uint8_t values[], uint8_t len);
 void ADXL345_init();
 int ADXL345_IsDataReady();
-void ADXL345_XYZ_Read(int16_t szData16[3]);
+void ADXL345_XYZ_Read(int16_t *);
 void ADXL345_IdRead(uint8_t *pId);
-int I2C0_onoff(int);
+int I2C0_onoff(unsigned int onoff);
 
+volatile sig_atomic_t stop;
 void catchSIGINT (int signum) {
 	printf("Unmapping\n");
-	mux_init(i2c0base);
-	unmap_physical(i2c0base, I2C0_SPAN);
-	close_physical(fd_i2c0base);
+	stop = 1;
 }
 
 int main(void) {
 
-	void * sysmgrbase; 
-	int fd_sysmgr = -1;
-	uint8_t devid;
-	uint8_t mg_per_lsb = 3.2;
-	uint16_t XYZ[3];
+	uint8_t devid = 0;
+	int16_t mg_per_lsb = 3.2;
+	int16_t XYZ[3];
+	stop = 0;
 
 	signal(SIGINT, catchSIGINT);
 	//Configure MUX to connect I2C0 controller to ADXL345
@@ -57,37 +58,47 @@ int main(void) {
 		return(-1);
 	}
 
-	if ((sysmgrbase = map_physical(fd_sysmgr, SYSMGR_BASE, SYSMGR_SPAN)) == NULL) {
+	if ((sysmgrbase_virtual = map_physical(fd_sysmgr, SYSMGR_BASE, SYSMGR_SPAN)) == NULL) {
 		return(-1);
 	}
 
-	printf("%p\n", sysmgrbase);
+	sysmgr_base_ptr = (unsigned int *) sysmgrbase_virtual;
+
+	printf("%p\n", sysmgr_base_ptr);
 	printf("Starting mux_init()\n");
-	mux_init(sysmgrbase);
+	mux_init();
 	printf("Finished mux_init()\n");
 
 	if ((fd_i2c0base = open_physical(fd_i2c0base)) == -1) {
 		return(-1);
 	}
 
-	if ((i2c0base = map_physical(fd_i2c0base, I2C0_BASE, I2C0_SPAN)) == NULL) {
+	if ((i2c0base_virtual = map_physical(fd_i2c0base, I2C0_BASE, I2C0_SPAN)) == NULL) {
 		return(-1);
 	}
 
-	base_ptr = (unsigned int *) i2c0base;
+	i2c0_base_ptr = (unsigned int *) i2c0base_virtual;
 	printf("Starting I2C0_Init()\n");
 	I2C0_Init();
 	printf("Getting ID\n");
-	ADXL345_REG_READ(ADXL345_DEVID, &devid);
+	ADXL345_IdRead(&devid);
 	printf("%#x\n", devid);
 	if (devid == 0xE5) {
 		printf("Found ADXL345\n");
+		ADXL345_init();
+		while(!stop) {
+			if(ADXL345_IsDataReady()) {
+				ADXL345_XYZ_Read(XYZ);
+				printf("X=%d mg, Y=%d mg, Z=%d mg\n", XYZ[0]*mg_per_lsb,
+					XYZ[1]*mg_per_lsb, XYZ[2]*mg_per_lsb);
+			}
+		}
 	}
 
 	//clean up
-	unmap_physical(i2c0base, I2C0_SPAN);
+	unmap_physical(i2c0base_virtual, I2C0_SPAN);
 	close_physical(fd_i2c0base);
-	unmap_physical(sysmgrbase, SYSMGR_SPAN);
+	unmap_physical(sysmgrbase_virtual, SYSMGR_SPAN);
 	close_physical(fd_sysmgr);
 
 	return 0;
@@ -132,32 +143,34 @@ void close_physical (int fd) {
 	close(fd);
 }
 
-void mux_init(void * sysmgrbase_ptr) {
+/* Set mux to connect ADXL345 to I2C0 Controller */
+void mux_init() {
 
 	volatile unsigned int *gpio7_ptr, *gpio8_ptr, *i2c0fpga_ptr; //Mux pointer
 
-	gpio7_ptr = (unsigned int *) (sysmgrbase_ptr + (SYSMGR_GENERALIO7 * 4));
-	gpio8_ptr = (unsigned int *) (sysmgrbase_ptr + (SYSMGR_GENERALIO8 * 4));
-	i2c0fpga_ptr = (unsigned int *) (sysmgrbase_ptr + (SYSMGR_I2C0USEFPGA * 4));
+	gpio7_ptr = sysmgr_base_ptr + SYSMGR_GENERALIO7;
+	gpio8_ptr = sysmgr_base_ptr + SYSMGR_GENERALIO8;
+	i2c0fpga_ptr =  sysmgr_base_ptr + SYSMGR_I2C0USEFPGA;
 
+	*i2c0fpga_ptr = 0;
 	*gpio7_ptr = 1;
 	*gpio8_ptr = 1;
-	*i2c0fpga_ptr = 0;
-	printf("gpio7_ptr: %#x\ngpio8_ptr: %#x\ni2c0fpga_ptr: %#x\n", *gpio7_ptr, *gpio8_ptr, *i2c0fpga_ptr);
+	printf("gio7: %#x\ngio8: %#x\ni2c0fpga: %#x\n", *gpio7_ptr, *gpio8_ptr, *i2c0fpga_ptr);
 }
 
-int I2C0_onoff(int onoff) {
+/* Enabling I2C0 with polling */
+int I2C0_onoff(unsigned int onoff) {
 	int ti2c_poll = 2500;
-	int MAX_T_POLL_COUNT = 10;
+	int MAX_T_POLL_COUNT = 100;
 	int poll_count = 0;
 
 	int good = 0;
-	*(base_ptr + (I2C0_ENABLE * 4)) = onoff + 1;
+	*(i2c0_base_ptr + I2C0_ENABLE) = onoff;
 
-	while (((*(base_ptr + (I2C0_ENABLE_STATUS * 4)) & 0x1) == onoff) & (poll_count < MAX_T_POLL_COUNT)) {
+	while (((*(i2c0_base_ptr + I2C0_ENABLE_STATUS) & 0x1) == (onoff - 1)) & (poll_count < MAX_T_POLL_COUNT)) {
 		poll_count++;
 		usleep(ti2c_poll);
-		*(base_ptr + (I2C0_ENABLE * 4)) = onoff + 1;
+		*(i2c0_base_ptr + I2C0_ENABLE) = onoff;
 	}
 	if (poll_count < 10)
 		good = 1;
@@ -168,77 +181,78 @@ int I2C0_onoff(int onoff) {
 int I2C0_Init() {
 
 	//Abort tranmission and disable Controller for config
-	*(base_ptr + (I2C0_ENABLE * 4)) = 0x2;
+	printf("%d\n%d\n", I2C0_TAR, I2C0_FS_SCL_HCNT * 1);
+	printf("%p\n%p\n", i2c0_base_ptr,  i2c0_base_ptr + (I2C0_FS_SCL_HCNT * 1));
+	*(i2c0_base_ptr + I2C0_ENABLE) = 0x2;
 	//Wait for disable status
 
-	if (!(I2C0_onoff(1))) {
+	if (!(I2C0_onoff(2))) {
 		printf("Unable to disable\n");
 		return 0;
 	}
 	printf("Configuring\n");
 
 	// 7-Bit addressing, FastMode (400kb/s), Master Mode
-	*(base_ptr + (I2C0_CON * 4)) = 0x65;
+	*(i2c0_base_ptr + I2C0_CON) = 0x65;
 
 	//Set target address to be ADXL345 devid 0x53 and 7bit addressing
-	*(base_ptr + (I2C0_TAR * 4)) = 0x53;
+	*(i2c0_base_ptr + I2C0_TAR) = 0x53;
 
 	//Minimum period is to be 2.5us but minimum high period is 0.6us
 	//and minimum low period is 1.3us so 0.3us is added to both.
-	*(base_ptr + (I2C0_FS_SCL_HCNT * 4)) = 60 + 30;
-	*(base_ptr + (I2C0_FS_SCL_LCNT * 4)) = 130 + 30;
+	*(i2c0_base_ptr + I2C0_FS_SCL_HCNT) = 60 + 30;
+	*(i2c0_base_ptr + I2C0_FS_SCL_LCNT) = 130 + 30;
 
 	//Enable the controller
-	*(base_ptr + (I2C0_ENABLE * 4)) = 0x1;
 	printf("Renabling controller\n");
-	if (!(I2C0_onoff(0))) {
+	if (!(I2C0_onoff(1))) {
 		printf("Unable to enable\n");
 		return 0;
 	}
-	printf("%#x\n", *(base_ptr + (I2C0_ENABLE_STATUS * 4)));
+	printf("ic_tar: %#x\nic_con: %#x\n", *(i2c0_base_ptr + I2C0_TAR), *(i2c0_base_ptr + I2C0_CON));
 	return 1;
 }
 
+/* Single Byte Read */
 void ADXL345_REG_READ(uint8_t address, uint8_t * value) {
 
 	//Send address and start signal
-	*(base_ptr + (I2C0_DATA_CMD * 4)) = address + 0x400;
+	*(i2c0_base_ptr + I2C0_DATA_CMD) = address + 0x400;
 
 	//send read signal
-	*(base_ptr + (I2C0_DATA_CMD * 4)) = 0x100;
+	*(i2c0_base_ptr + I2C0_DATA_CMD) = 0x100;
 
 	//wait for response
-	while(*(base_ptr + (I2C0_RXFLR * 4)) == 0) {
-		usleep(500000);
-		*(base_ptr + (I2C0_DATA_CMD * 4)) = 0x100;
-		printf("%#x\n", *(base_ptr + (I2C0_RXFLR * 4)));
-	}
 
-	*value = *(base_ptr + (I2C0_DATA_CMD * 4)) | 0xFF;
+	while(*(i2c0_base_ptr + I2C0_RXFLR) == 0) {
+		continue;
+	}
+	*value = *(i2c0_base_ptr + I2C0_DATA_CMD) & 0xFF;
 }
 
+/* Single byte Write */
 void ADXL345_REG_WRITE(uint8_t address, uint8_t value) {
 
-	*(base_ptr + (I2C0_DATA_CMD * 4)) = address + 0x400;
-
-	*(base_ptr + (I2C0_DATA_CMD * 4)) = value;
+	*(i2c0_base_ptr + I2C0_DATA_CMD) = address + 0x400;
+	*(i2c0_base_ptr + I2C0_DATA_CMD) = value;
 }
 
+/* Multiple Byte Write */
 void ADXL345_REG_MULTI_READ(uint8_t address, uint8_t values[], uint8_t len) {
 
 	int i = 0;
 	int nth_byte = 0;
-	*(base_ptr + (I2C0_DATA_CMD * 4)) = address + 0x400;
+	*(i2c0_base_ptr + I2C0_DATA_CMD) = address + 0x400;
 
 	//send read signal multiple times to prevent overwritten data at 
 	//inconsistent times
 
 	for(i = 0; i < len; i++)
-		*(base_ptr + (I2C0_DATA_CMD * 4)) = 0x100;
+		*(i2c0_base_ptr + I2C0_DATA_CMD) = 0x100;
 
 	while(len) {
-		if (*(base_ptr + (I2C0_RXFLR * 4)) > 0) {
-			values[nth_byte] = *(base_ptr + (I2C0_DATA_CMD * 4)) | 0xFF;
+		if (*(i2c0_base_ptr + I2C0_RXFLR) > 0) {
+			values[nth_byte] = *(i2c0_base_ptr + I2C0_DATA_CMD) & 0xFF;
 			nth_byte++;
 			len--;
 		}
@@ -259,6 +273,7 @@ void ADXL345_init() {
 	ADXL345_REG_WRITE(ADXL345_THRESH_INACT, 0x02);
 	ADXL345_REG_WRITE(ADXL345_TIME_INACT, 0x02);
 	ADXL345_REG_WRITE(ADXL345_ACT_INACT_CTL, 0xFF);
+	ADXL345_REG_WRITE(ADXL345_INT_ENABLE, 0x18);
 
 	//Reset Measurement config
 	ADXL345_REG_WRITE(ADXL345_POWER_CTL, 0x00); //standby
@@ -270,9 +285,12 @@ int ADXL345_IsDataReady() {
 	uint8_t data8;
 
 	ADXL345_REG_READ(ADXL345_INT_SOURCE, &data8);
-	if (data8 & 0x08)
-		bReady = 1;
-
+	if (data8 & ADXL345_ACTIVITY) {
+		ADXL345_REG_READ(ADXL345_INT_SOURCE, &data8);
+		if (data8 & ADXL345_DATAREADY) {
+			bReady = 1;
+		}
+	}
 	return bReady;
 }
 
